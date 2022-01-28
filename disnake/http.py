95e84e103ad9080fb7d9769890ca53e26e0110bf
onerandomusername <genericusername414+git@gmail.com>
2022-01-28 00:10:53 -0500

    chore: don't use mutable defaults in function definitions
    mutable defaults are persisted between calls which means edits
    persist the next time the function is called

--- disnake/http.py
+++ disnake/http.py
@@ -2411,7 +2411,7 @@ class HTTPClient:
         self,
         application_id: Snowflake,
         token: str,
-        files: List[File] = [],
+        files: List[File] = None,
         content: Optional[str] = None,
         tts: bool = False,
         embeds: Optional[List[embed.Embed]] = None,
@@ -2423,6 +2423,8 @@ class HTTPClient:
             application_id=application_id,
             interaction_token=token,
         )
+        if files is None:
+            files = []
         return self.send_multipart_helper(
             r,
             content=content,
